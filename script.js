3f005e184daff928a9cd37241f5b4c418680b0bb
Max Strikanov <maxvell.88@mail.ru>
2020-08-31 18:19:39 +0300

    Дополнительно!

--- script.js
+++ script.js
@@ -12,38 +12,55 @@ function DomElement( selector, height, width, bg, fontSize ){
 DomElement.prototype.createEl = function() {
 
   if ( this.selector.charAt(0) === '.' ) {
+
   let elementDiv;
+
     elementDiv = document.createElement('div');
     elementDiv.className = `${this.selector.slice(1)}`;
     elementDiv.style.cssText = `
+    position: absolute;
     height:${this.height}px;
     width:${this.width}px;
     background:${this.bg};
     font-size:${this.fontSize}px;`;
     elementDiv.innerHTML = 'JAVA SCRIPT';
+
     return elementDiv;
     
   };
 
   if ( this.selector.charAt(0) === '#' ) {
+
     let elementP;
+
     elementP = document.createElement('p');
     elementP.id = `#${this.selector.slice(1)}`;
     elementP.style.cssText = `
+    position: absolute;
     height:${this.height}px;
     width:${this.width}px;
     background:${this.bg};
     font-size:${this.fontSize}px;`;
     elementP.innerHTML = 'JAVA SCRIPT';
+    
     return elementP;
   };
 
 };
 
-let newEl = new DomElement('#block', '50', '100', 'yellow', '18' );
+let newEl = new DomElement('.block', '100', '100', 'yellow', '18' );
 
 document.body.append(newEl.createEl());
 
 console.log(newEl.createEl());
 
+document.addEventListener('keydown', (e) => {
+  
+   let element = newEl.createEl();
 
+  (e.code == 'ArrowUp') ? element.style.marginTop = '-10px': 
+  (e.code == 'ArrowDown') ? element.style.marginBottom = '-10px': 
+  (e.code == 'ArrowLeft') ? element.style.marginLeft = '-10px': 
+  element.style.marginRight = '-10px';
+  
+})
